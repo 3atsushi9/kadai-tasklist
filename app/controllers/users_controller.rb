b3fc15cacae4f1420a5f2d5ed133c8d10627ae2c
mahone <onebase2310a@gmail.com>
2020-09-14 11:13:13 +0000

    ログインの途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
@@ -22,11 +21,9 @@ class UsersController < ApplicationController
     end
   end
   
-  
-  private
+   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
-
