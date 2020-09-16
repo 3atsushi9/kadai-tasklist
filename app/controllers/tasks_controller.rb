4bdf8db6569b7b7a12460e52c0e6d40861fa04da
mahone <onebase2310a@gmail.com>
2020-09-16 06:02:22 +0000

    Lesson15課題①修正２

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = "Taskは正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -62,7 +62,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+      redirect_to login_url
     end
   end
 end
