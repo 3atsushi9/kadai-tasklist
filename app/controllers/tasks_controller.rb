4e92f26097a8fcac41ebd644d5d8eae196029472
mahone <onebase2310a@gmail.com>
2020-09-16 06:29:23 +0000

    課題修正２続き

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = "Taskが作成されませんでした"
-      render :'tasks/index'
+      render :'new'
     end
   end
   
